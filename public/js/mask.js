d1f91e59fd2fa3b2b2929d002b4c9bb98f12caa0
Vitor Parisio <vitorparisio@yahoo.com.br>
2023-10-26 10:52:02 -0300

    Correcao do estoque minimo para formato inteiro

--- public/js/mask.js
+++ public/js/mask.js
@@ -3,10 +3,8 @@ $(function(){
     $('.preco_compra_editar').mask("000.000.000.000.000,00", {reverse: true});
     $('.preco_venda_editar').mask("000.000.000.000.000,00", {reverse: true});
     $('.preco_minimo_editar').mask("000.000.000.000.000,00", {reverse: true});
-    $('#estoque_minimo_editar').mask("000.000.000.000.000,00", {reverse: true});
     $('#preco_compra').mask("000.000.000.000.000,00", {reverse: true});
     $('#preco_venda').mask("000.000.000.000.000,00", {reverse: true});
     $('#vuntrib').mask("000.000.000.000.000,00", {reverse: true});
     $('#preco_minimo').mask("000.000.000.000.000,00", {reverse: true});
-    $('#estoque_minimo').mask("000.000.000.000.000,00", {reverse: true});
 });
